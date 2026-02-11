cf0a6a3257940a32b73458a3fe2d18d04ead8cfa
JakeSanderson350 <jake.sanderson@mymail.champlain.edu>
2026-02-10 21:29:02 -0500

    Created destructible wall and projectile debug scripts

---
+++ Assets/Scripts/ProjectileLauncher.cs
@@ -0,0 +1,52 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class ProjectileLauncher : MonoBehaviour
+{
+    [Header("Projectile Settings")]
+    public float impactForce = 100.0f;
+    public float impactRadius = 1.0f;
+
+    private Camera cam;
+
+    public void Awake()
+    {
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            TryFireAtWall();
+    }
+
+    private void TryFireAtWall()
+    {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, 100.0f))
+        {
+            DestructibleWall wall = hit.collider.GetComponent<DestructibleWall>();
+            if (wall == null)
+                return;
+
+            ImpactData impact = new ImpactData
+            {
+                worldPos = hit.point,
+                localPos = wall.transform.InverseTransformPoint(hit.point),
+                force = impactForce,
+                radius = impactRadius
+            };
+
+
+        }
+    }
+}
+
+public struct ImpactData
+{
+    public Vector3 worldPos;
+    public Vector3 localPos;
+    public float force;
+    public float radius;
+}
---
+++ Assets/Scripts/Wall/DestructibleWall.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class DestructibleWall : MonoBehaviour
+{
+    [Header("Fracture Settings")]
+    public int numChunks = 16;
+
+    public void RecieveImpact(ImpactData impact)
+    {
+
+    }
+}
